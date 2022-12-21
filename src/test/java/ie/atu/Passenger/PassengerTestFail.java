859e6bad5c19f5fc0927fa93fe6ff93eeed7861a
Canhebeatg0kutho <G00382880@atu.ie>
2022-12-21 23:15:23 +0000

    Added test for phone number failure

--- src/test/java/ie/atu/Passenger/PassengerTestFail.java
+++ src/test/java/ie/atu/Passenger/PassengerTestFail.java
@@ -30,6 +30,13 @@ class PassengerTestFail {
         assertEquals("This is not a valid ID.", exMessage.getMessage());
     }
 
+    @Test
+    void testPhoneFail(){
+        Exception exMessage = assertThrows(IllegalArgumentException.class, () -> {new Passenger("Mr", "John Doe", 1235423221213L, 122222, 19);});
+        assertEquals("This is not a valid phone number.", exMessage.getMessage());
+    }
+
+
 
 
     @AfterEach
