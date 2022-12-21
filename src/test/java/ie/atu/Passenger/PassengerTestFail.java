f9fc2630221b3d612b8c3ad90cb24eb31f927f08
Canhebeatg0kutho <G00382880@atu.ie>
2022-12-21 23:14:09 +0000

    Added test for title failure

--- src/test/java/ie/atu/Passenger/PassengerTestFail.java
+++ src/test/java/ie/atu/Passenger/PassengerTestFail.java
@@ -2,6 +2,7 @@ package ie.atu.Passenger;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,6 +11,13 @@ class PassengerTestFail {
     @BeforeEach
     void setUp() {
     }
+
+    @Test
+    void testTitleFail(){
+        Exception exMessage = assertThrows(IllegalArgumentException.class, () -> {new Passenger("Mister", "John Doe", 1235473822, 1222222223, 19);});
+        assertEquals("This is not a valid name. Use Mr, Ms or Mrs.", exMessage.getMessage());
+    }
+
 
     @AfterEach
     void tearDown() {
