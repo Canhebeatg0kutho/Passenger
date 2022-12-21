c6ab0309c7d892bee4d5c427c13fd936beb93817
Canhebeatg0kutho <G00382880@atu.ie>
2022-12-21 22:52:38 +0000

    Created variables

--- src/main/java/ie/atu/Passenger/Passenger.java
+++ src/main/java/ie/atu/Passenger/Passenger.java
@@ -1,4 +1,11 @@
 package ie.atu.Passenger;
 
 public class Passenger {
+    private String title;
+    private String name;
+    private long id;
+    private long phone;
+    private int age;
+
+
 }
