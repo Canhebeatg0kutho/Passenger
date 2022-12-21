07265238a5cda0d185bfa94c25f151112d4b33e2
Canhebeatg0kutho <G00382880@atu.ie>
2022-12-21 23:01:10 +0000

    Created passenger object

--- src/test/java/ie/atu/Passenger/PassengerTest.java
+++ src/test/java/ie/atu/Passenger/PassengerTest.java
@@ -7,8 +7,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PassengerTest {
 
+    private Passenger myPassenger;
+
     @BeforeEach
     void setUp() {
+        myPassenger = new Passenger("Mr", "John Doe", 1234334567891L, 1233456789, 42);
+
     }
 
     @AfterEach
